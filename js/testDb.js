b2a63ce94cbb779f4f62e1e9cdcd77aa8339f0ba
Sylvain Janet <sjanet@jehann.fr>
2022-11-29 21:11:40 +0100

    added both http and https

--- js/testDb.js
+++ js/testDb.js
@@ -16,30 +16,60 @@ try {
 
 try {
   document.getElementById("testDbrealIP").textContent = httpGet(
-    "https://31.187.72.184:8080/test/hello"
+    "http://31.187.72.184:8080/test/hello"
   );
-  console.log("Real backend with IP OK");
+  console.log("Real backend with IP OK - http");
 } catch (error) {
-  console.log("Real backend not accessed with IP");
+  console.log("Real backend not accessed with IP - http");
   console.log(error);
 }
 
 try {
   document.getElementById("testDbrealPort").textContent = httpGet(
-    "https://server.sylvainjanet.fr:8080/test/hello"
+    "http://server.sylvainjanet.fr:8080/test/hello"
   );
-  console.log("Real backend with adress and port OK");
+  console.log("Real backend with adress and port OK - http");
 } catch (error) {
-  console.log("Real backend not accessed with adress and port");
+  console.log("Real backend not accessed with adress and port - http");
   console.log(error);
 }
 
 try {
   document.getElementById("testDbreal").textContent = httpGet(
+    "http://server.sylvainjanet.fr/test/hello"
+  );
+  console.log("Real backend with adress OK - http");
+} catch (error) {
+  console.log("Real backend not accessed with adress - http");
+  console.log(error);
+}
+
+try {
+  document.getElementById("testDbrealIPhttps").textContent = httpGet(
+    "https://31.187.72.184:8080/test/hello"
+  );
+  console.log("Real backend with IP OK - https");
+} catch (error) {
+  console.log("Real backend not accessed with IP - https");
+  console.log(error);
+}
+
+try {
+  document.getElementById("testDbrealPorthttps").textContent = httpGet(
+    "https://server.sylvainjanet.fr:8080/test/hello"
+  );
+  console.log("Real backend with adress and port OK - https");
+} catch (error) {
+  console.log("Real backend not accessed with adress and port - https");
+  console.log(error);
+}
+
+try {
+  document.getElementById("testDbrealhttps").textContent = httpGet(
     "https://server.sylvainjanet.fr/test/hello"
   );
-  console.log("Real backend with adress OK");
+  console.log("Real backend with adress OK - https");
 } catch (error) {
-  console.log("Real backend not accessed with adress");
+  console.log("Real backend not accessed with adress - https");
   console.log(error);
 }
