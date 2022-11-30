e58c140e56575d056edfefd00040a95a89132ebd
Sylvain Janet <sjanet@jehann.fr>
2022-11-30 02:36:55 +0100

    only relevant adresses

--- js/testDb.js
+++ js/testDb.js
@@ -15,46 +15,6 @@ try {
 }
 
 try {
-  document.getElementById("testDbrealIP").textContent = httpGet(
-    "http://31.187.72.184:8080/test/hello"
-  );
-  console.log("Real backend with IP OK - http");
-} catch (error) {
-  console.log("Real backend not accessed with IP - http");
-  console.log(error);
-}
-
-try {
-  document.getElementById("testDbrealPort").textContent = httpGet(
-    "http://server.sylvainjanet.fr:8080/test/hello"
-  );
-  console.log("Real backend with adress and port OK - http");
-} catch (error) {
-  console.log("Real backend not accessed with adress and port - http");
-  console.log(error);
-}
-
-try {
-  document.getElementById("testDbreal").textContent = httpGet(
-    "http://server.sylvainjanet.fr/test/hello"
-  );
-  console.log("Real backend with adress OK - http");
-} catch (error) {
-  console.log("Real backend not accessed with adress - http");
-  console.log(error);
-}
-
-try {
-  document.getElementById("testDbrealIPhttps").textContent = httpGet(
-    "https://31.187.72.184:8443/test/hello"
-  );
-  console.log("Real backend with IP OK - https");
-} catch (error) {
-  console.log("Real backend not accessed with IP - https");
-  console.log(error);
-}
-
-try {
   document.getElementById("testDbrealPorthttps").textContent = httpGet(
     "https://server.sylvainjanet.fr:8443/test/hello"
   );
